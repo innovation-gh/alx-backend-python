44e60f452a51bb2f6acaae6b3025fff077dcb612
Joseph Olaitan Felix <destechinnovation@gmail.com>
2025-07-03 04:14:40 +0000

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,28 +1,26 @@
 import mysql.connector
 
 def stream_users_in_batches(batch_size):
-    conn = mysql.connector.connect(
+    connection = mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
         database="ALX_prodev"
     )
-    try:
-        cursor = conn.cursor(dictionary=True)
-        offset = 0
-        while True:
-            cursor.execute(
-                "SELECT user_id, name, email, age FROM user_data LIMIT %s OFFSET %s",
-                (batch_size, offset)
-            )
-            batch = cursor.fetchall()
-            if not batch:
-                break
-            yield batch
-            offset += batch_size
-    finally:
-        cursor.close()
-        conn.close()
+    cursor = connection.cursor(dictionary=True)
+    offset = 0
+    while True:
+        cursor.execute(
+            "SELECT user_id, name, email, age FROM user_data LIMIT %s OFFSET %s",
+            (batch_size, offset)
+        )
+        batch = cursor.fetchall()
+        if not batch:
+            break
+        yield batch
+        offset += batch_size
+    cursor.close()
+    connection.close()
 
 def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):
