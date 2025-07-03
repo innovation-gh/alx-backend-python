faffe96f56479abb4cab981410d20bbb506731a1
Joseph Olaitan Felix <destechinnovation@gmail.com>
2025-07-03 03:53:39 +0000

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -4,7 +4,7 @@ def stream_users_in_batches(batch_size):
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",  # your MySQL password
+        password="",
         database="ALX_prodev"
     )
     try:
