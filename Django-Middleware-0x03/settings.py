28c21e033202e1dd4163d0884045d15c9623d0ab
Joseph Olaitan Felix <destechinnovation@gmail.com>
2025-08-01 05:06:31 +0000

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -9,7 +9,7 @@ MIDDLEWARE = [
     'chats.middleware.RequestLoggingMiddleware',  # Request logging middleware
     'chats.middleware.RestrictAccessByTimeMiddleware',  # Time restriction middleware
     'chats.middleware.OffensiveLanguageMiddleware',  # Rate limiting middleware
-    'chats.middleware.RolePermissionMiddleware',  # Role-based access control middleware
+    'chats.middleware.RolepermissionMiddleware',  # Role-based access control middleware
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
